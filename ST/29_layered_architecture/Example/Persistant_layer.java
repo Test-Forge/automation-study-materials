a3e96e438cf5a7f8f9d3570d0af0976dc5ecf30a
AnMalaia <amalaia2122>
2024-01-24 09:51:49 +0200

    Create project folder for layered architecture theme  + README file

---
+++ ST/29_layered_architecture/Example/Persistant_layer.java
@@ -0,0 +1,6 @@
+The persistence layer, also called the data access layer, acts as a protective layer. It contains the code that's
+        necessary to access the database layer. This layer also holds the set of codes that allow you to manipulate
+        various aspects of the database, such as connection details and SQL statements.
+
+Persistence/Data Access Layer - manages the storage and retrieval of data from databases or other data sources.
+        It provides the necessary mechanisms to interact with the underlying data storage systems
