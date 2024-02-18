2e875f6b9b5077e9eb2c6b9bd30e716f03a87ceb
IgChele <152602453+IgChele@users.noreply.github.com>
2024-02-18 17:14:13 +0200

    WebDriver configuration : README file + examples (#20)
    * WebDriver config
    
    * Adjusted file format

---
+++ TL/25_WebDriver_configuration/BasicConfiguration/JUnitExample/NavigationTest.java
@@ -0,0 +1,27 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigationTest {
+
+    @Test
+    public void navigationTest() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/documentation/webdriver/");
+        driver.manage().window().maximize();
+
+        WebElement btnBrowsers = driver.findElement(By.id("m-documentationwebdriverbrowsers"));
+        btnBrowsers.click();
+
+        String currentPage = driver.getCurrentUrl();
+        Assert.assertEquals("https://www.selenium.dev/documentation/webdriver/browsers/", currentPage);
+
+        System.out.println("Page title: " + driver.getTitle());
+
+        driver.quit();
+    }
+}
