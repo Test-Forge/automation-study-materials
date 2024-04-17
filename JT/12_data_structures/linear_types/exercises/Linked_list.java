7f35f48624f16dadafd740aed891666892c2aee1
anamalaiaEndava <96619181+anamalaiaEndava@users.noreply.github.com>
2024-04-17 10:43:54 +0300

    12_Data Structures (#52)

---
+++ JT/12_data_structures/linear_types/exercises/Linked_list.java
@@ -0,0 +1,17 @@
+/*
+1. Easy level
+Write a Java program to insert elements into the linked list at the first and last position
+
+2. Medium level
+    Given a linked list, in which each node contains an integer value.
+    Between every pair of adjacent nodes, insert a new node with a value equal to the greatest common divisor of them.
+    Return the linked list after insertion.
+    Note: The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+*/
+
+public class Linked_list {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ JT/12_data_structures/linear_types/exercises/Queue.java
@@ -0,0 +1,25 @@
+/*
+Basic Operations of Queue Data Structure
+        - Enqueue (Insert): Adds an element to the rear of the queue.
+        - Dequeue (Delete): Removes and returns the element from the front of the queue.
+        - Peek: Returns the element at the front of the queue without removing it.
+        - isEmpty: Checks if the queue is empty.
+        - isFull: Checks if the queue is full.
+
+1. Easy level
+    Write a program to remove an item from the Queue
+    Create a Queue (imlement using a Linked List), add 7 integer elements, remove the last number
+    and print the resulting Queue
+
+2. Medium level
+    The Java code checks whether the queue of characters is a palindrome or not.
+    A palindrome is a sequence of characters that reads the same backward as forward.
+    Try to resolve task using two queues
+*/
+
+public class Queue {
+
+    public static void main(String[] args) {
+
+    }
+}
