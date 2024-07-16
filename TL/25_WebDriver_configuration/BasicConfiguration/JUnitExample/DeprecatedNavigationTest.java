41aa51e56a5ccb9b752b12c3a73f763149812bef
IgorGorea <40035632+IgorGorea@users.noreply.github.com>
2024-07-16 12:38:36 +0300

    25_WebDriver_configuration revised and improved. 'README.adoc' file(previously Intro file) updated. Example renamed. (#73)

--- TL/25_WebDriver_configuration/BasicConfiguration/JUnitExample/DeprecatedNavigationTest.java
+++ TL/25_WebDriver_configuration/BasicConfiguration/JUnitExample/DeprecatedNavigationTest.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class NavigationTest {
+public class DepricatedNavigationTest {
 
     @Test
     public void navigationTest() {
